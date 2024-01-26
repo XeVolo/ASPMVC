93ea5464a7a6a30821320841d19c9bcaf01d37d3
xevolo <xevolo12@gmail.com>
2024-01-26 20:26:18 +0100

    kategorie

--- Models/CategoryModel.cs
+++ Models/CategoryModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +10,14 @@ namespace ASPMVC.Models
 	public class CategoryModel
 	{
 		public int Id { get; set; }
-		[Required]
 		public string Name { get; set; }
+		public int? ParentCategoryId { get; set; }
+		public int Left { get; set; }
+		public int Right { get; set; }
+
+		[ForeignKey("ParentCategoryId")]
+		public virtual CategoryModel ParentCategory { get; set; }
+
+		public virtual ICollection<CategoryModel> Subcategories { get; set; }
 	}
 }
