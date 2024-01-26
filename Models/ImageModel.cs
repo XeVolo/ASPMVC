a74fff6ed430ffed14dc3d012b295410fe19aef9
Mixus <mateuszokragly@gmail.com>
2024-01-26 20:19:30 +0100

    Tabela do zdjęć

---
+++ Models/ImageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ASPMVC.Models
+{
+    public class ImageModel
+    {
+        public int Id { get; set; }
+        public int SaleAnnouncementId { get; set; }
+        public string Path { get; set; }
+        [ForeignKey("SaleAnnouncementId")]
+        public virtual SaleAnnouncementModel SaleAnnouncementModel { get; set;}
+    }
+}
