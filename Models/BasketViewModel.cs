85ca13d1900c7fab4d2bd49d2f2d90564e385e05
xevolo <xevolo12@gmail.com>
2024-01-26 19:50:46 +0100

    ssss

---
+++ Models/BasketViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPMVC.Models
+{
+	public class BasketViewModel
+	{
+		public int ProductId { get; set; }
+
+		public string Name { get; set; }
+		public double Price { get; set; }
+		public int Quantity { get; set; }
+	}
+}
